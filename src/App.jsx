0b78685ef122be3b95d719ac69f4730e7f355e2c
gsaple <yymelau@gmail.com>
2024-09-04 13:47:17 +1000

    add rotation

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,27 @@ const DIMENSION = 5;
 
 function App() {
   const [position, setPosition] = useState({ x: 0, y: 0, direction: 0 });
+
+  const rotateAnticlockwise = () => {
+    setPosition({
+      ...position,
+      direction: (position.direction + 3) % 4,
+    });
+  };
+
+  const rotateClockwise = () => {
+    setPosition({
+      ...position,
+      direction: (position.direction + 1) % 4,
+    });
+  };
   return (
     <>
       <h1>Robot Simulator</h1>
       <div className="buttons">
-        <button>Rotate Left</button>
+        <button onClick={rotateAnticlockwise}>Rotate Anticlockwise</button>
         <button>Move Forward</button>
-        <button>Rotate Right</button>
+        <button onClick={rotateClockwise}>Rotate Clockwise</button>
       </div>
       <div className="grid">
         {[...Array(DIMENSION)].map((_, rowIndex) => (
