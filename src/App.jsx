98c26981fc8e12f8f68fe97f822ca5e7cf9beec5
gsaple <yymelau@gmail.com>
2024-09-04 12:03:33 +1000

    vite skeleton
    vite skeleton

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Robot Simulator</h1>
+      <div className="buttons">
+        <button>Rotate Left</button>
+        <button>Move Forward</button>
+        <button>Rotate Right</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
