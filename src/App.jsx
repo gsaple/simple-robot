b096f361bfe39e4f45f91067544dde5d225074df
gsaple <yymelau@gmail.com>
2024-09-04 14:03:10 +1000

    move forward

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,40 @@ function App() {
       direction: (position.direction + 1) % 4,
     });
   };
+
+  const moveForward = () => {
+    let { x, y, direction } = position;
+
+    switch (DIRECTIONS[direction]) {
+      case "EAST":
+        x = Math.min(DIMENSION - 1, x + 1);
+        break;
+      case "SOUTH":
+        y = Math.min(DIMENSION - 1, y + 1);
+        break;
+      case "WEST":
+        x = Math.max(0, x - 1);
+        break;
+      case "NORTH":
+        y = Math.max(0, y - 1);
+        break;
+      default:
+        break;
+    }
+
+    setPosition({
+      ...position,
+      x: x,
+      y: y,
+    });
+  };
+
   return (
     <>
       <h1>Robot Simulator</h1>
       <div className="buttons">
         <button onClick={rotateAnticlockwise}>Rotate Anticlockwise</button>
-        <button>Move Forward</button>
+        <button onClick={moveForward}>Move Forward</button>
         <button onClick={rotateClockwise}>Rotate Clockwise</button>
       </div>
       <div className="grid">
